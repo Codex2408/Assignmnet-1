de2139c432a6f286580314c6d89e2151c37b0996
Codex2408 <aroraarvin9@gmail.com>
2025-08-21 17:22:42 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+a=int(input("Enter 1st number:"))
+b=int(input("Enter 2nd number:"))
+sum=a+b
+diff=a-b
+mul=a*b
+div=a/b
+print("The sum is",sum)
+print("The difference is",diff)
+print("The multiplication is",mul)
+print("The division is",div)
+print("Thank You")
+
---
+++ Task_2.py
@@ -0,0 +1,4 @@
+f=input("Enter Your First Name: ")
+d=input("Enter Your Last Name: ")
+name=f+" "+d
+print("Hello,",name,"! Welcome to Python")
